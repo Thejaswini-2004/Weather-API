9e1f5793d8b6b236c8a2504ec7be830b03e9f0c5
Thejaswini-2004 <thejaswinir01@gmail.com>
2026-04-03 07:59:00 +0530

    updated the config

--- weather-api/app/config.py
+++ weather-api/app/config.py
@@ -1,7 +1,7 @@
-import os
-from dotenv import load_dotenv
+import os  #Used to access environment variables
+from dotenv import load_dotenv   #Loads variables from .env file
 
-load_dotenv()
+load_dotenv()  #Reads .env and makes variables accessible
 
 API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL")

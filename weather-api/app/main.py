17c7cc051a0d5bd8adee8ff2b01f76dda30ef204
Thejaswini-2004 <thejaswinir01@gmail.com>
2026-04-10 15:54:59 +0530

    added gitignore

--- weather-api/app/main.py
+++ weather-api/app/main.py
@@ -5,3 +5,6 @@ from app.routes import router
 app = FastAPI()
 
 app.include_router(router)
+@app.get("/")
+def home():
+    return {"message": "Weather API is running"}
--- weather-api/app/services.py
+++ weather-api/app/services.py
@@ -1,18 +1,19 @@
 # app/services.py
-import requests
-from app.config import API_KEY, BASE_URL
+import requests  #requests helps to interact with APIs   
+from app.config import API_KEY, BASE_URL  #reusing variables from config
 
 def get_weather(city: str):
     url = f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
 
     if response.status_code != 200:
-        return {"error": "City not found"}
+        return {"error": "City not found"}  #Error Handling
 
-    data = response.json()
+    data = response.json()  #Helps to convert raw data from the api to list or dictionary in this case its dictionary
 
     return {
         "city": data["name"],
         "temperature": data["main"]["temp"],
         "weather": data["weather"][0]["description"]
     }
+    #returns only the specified data from the response
